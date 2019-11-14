09b0e360f8c0a79da2b7a602475153c18603ebcd
Fahad Ahmad <fahadboss10@gmail.com>
2019-11-14 16:29:57 +0500

    Variables program

---
+++ Variables/src/com/fahad/variables/Main.java
@@ -0,0 +1,12 @@
+package com.fahad.variables;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String myName = "Fahad Ahmad";
+        byte myAge = 22;
+        char nameInitial = 'F';
+
+        System.out.println("Hello! My name is " + myName + ". I'm " + myAge + " years old. My name initial is '" + nameInitial + "'.");
+    }
+}
